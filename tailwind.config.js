a0379f78940d926362eb9b01473c452d85f49bde
uzuriel <seeaanlb@gmail.com>
2024-11-17 19:00:35 +0800

    Configured custom fonts, screen sizes, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'colorPrimary': '#111928',
+        'colorSecondary': '#2D3646',
+      },
+      screens: {
+        'phone': '320px',
+        'tablet': '480px',
+        'desktop': '770px',
+      },
+      fontFamily: {
+        inter: ['Inter']
+      },
+    },
   },
   plugins: [],
 };
