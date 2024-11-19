9c54b33bcda9a946a4fb67001cbbf45827ff3986
Naxus <98635973+SpottedYodeler2@users.noreply.github.com>
2024-11-20 06:12:56 +0800

    Revert "feat/ navbar and footer"
    This reverts commit 5acfd23d29beeba0cd8aceff3a9633df3b73ba5e.

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
     <main className="w-full bg-colorPrimary h-screen py-16 px-24">
-      <NavBar />
+      {/* Add navbar */}
       <Outlet />
-      <Footer />
+      {/* Add footer */}
     </main>
   );
 };
