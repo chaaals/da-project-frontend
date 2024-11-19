704678b98d5bb8f23627d9f26b1fc2a85494ee8f
uzuriel <seeaanlb@gmail.com>
2024-11-19 16:55:52 +0800

    header, details and button of import section and uploading function

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ export default {
       colors: {
         'colorPrimary': '#111928',
         'colorSecondary': '#2D3646',
+        'textPrimary': '#CBCBCB',
+        'textSecondary': '#8C9199'
       },
       screens: {
         'phone': '320px',
