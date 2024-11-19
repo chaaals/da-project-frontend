5acfd23d29beeba0cd8aceff3a9633df3b73ba5e
Naxus <98635973+SpottedYodeler2@users.noreply.github.com>
2024-11-20 06:11:03 +0800

    feat/ navbar and footer
    fixes #5 and #6

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
     <main className="w-full bg-colorPrimary h-screen py-16 px-24">
-      {/* Add navbar */}
+      <NavBar />
       <Outlet />
-      {/* Add footer */}
+      <Footer />
     </main>
   );
 };
