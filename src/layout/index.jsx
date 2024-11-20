b4a99007a6ed0e653d6b6eb4d3ccbdd0e2d33c69
uzuriel <seeaanlb@gmail.com>
2024-11-20 13:42:00 +0800

    Fixed background color expanding varies in window sizes

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -2,11 +2,12 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <main className="w-full bg-colorPrimary h-screen py-16 px-24">
-      {/* Add navbar */}
-      <Outlet />
-      {/* Add footer */}
+    <main className="w-full bg-colorPrimary h-screen py-16 px-24 overflow-auto">
+        {/* Add navbar */}
+        <Outlet />
+        {/* Add footer */}
     </main>
+
   );
 };
 
