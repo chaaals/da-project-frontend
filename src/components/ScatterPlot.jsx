c56df718d3b001a4b9301a04d2eb565c3567927c
chaaals <charlesching0808@gmail.com>
2024-12-02 05:06:38 +0800

    feat: modified scatter plot component

--- src/components/ScatterPlot.jsx
+++ src/components/ScatterPlot.jsx
@@ -1,54 +1,46 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-const ScatterPlot = ({ visId, data, options = {} }) => {
-  const selector = `scatter-${visId}`;
+const ScatterPlot = ({ data, width = 500, height = 400 }) => {
+  const ref = useRef();
 
   useEffect(() => {
-    const {
-      width = 600,
-      height = 400,
-      margin = { top: 20, right: 20, bottom: 30, left: 40 },
-    } = options;
-
     const svg = d3
-      .select(`#${selector}`)
-      .append("svg")
+      .select(ref.current)
       .attr("width", width)
       .attr("height", height);
 
-    const x = d3
+    const xScale = d3
       .scaleLinear()
-      .domain(d3.extent(data, (d) => d.x))
-      .nice()
-      .range([margin.left, width - margin.right]);
-    const y = d3
+      .domain([0, d3.max(data, (d) => d.x)])
+      .range([40, width - 40]);
+
+    const yScale = d3
       .scaleLinear()
-      .domain(d3.extent(data, (d) => d.y))
-      .nice()
-      .range([height - margin.bottom, margin.top]);
+      .domain([0, d3.max(data, (d) => d.y)])
+      .range([height - 40, 40]);
 
     svg
-      .append("g")
-      .attr("transform", `translate(0,${height - margin.bottom})`)
-      .call(d3.axisBottom(x));
-    svg
-      .append("g")
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(d3.axisLeft(y));
-
-    svg
-      .append("g")
       .selectAll("circle")
       .data(data)
       .join("circle")
-      .attr("cx", (d) => x(d.x))
-      .attr("cy", (d) => y(d.y))
+      .attr("cx", (d) => xScale(d.x))
+      .attr("cy", (d) => yScale(d.y))
       .attr("r", 5)
-      .attr("fill", "steelblue");
-  }, [data, options, selector]);
+      .attr("fill", "teal");
 
-  return <div id={selector}></div>;
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${height - 40})`)
+      .call(d3.axisBottom(xScale));
+
+    svg
+      .append("g")
+      .attr("transform", `translate(40, 0)`)
+      .call(d3.axisLeft(yScale));
+  }, [data, width, height]);
+
+  return <svg ref={ref}></svg>;
 };
 
 export default ScatterPlot;
