e85e9a660070c38c21d560c7b9f94691af1c3d6e
chaaals <charlesching0808@gmail.com>
2024-12-02 02:33:33 +0800

    feat: add backend queries for page

---
+++ src/queries/page.js
@@ -0,0 +1,41 @@
+const BASE_URL = import.meta.env.VITE_DEV_API;
+
+export const getPages = async (reportId) => {
+  try {
+    const res = await fetch(BASE_URL + `${reportId}/page/`, {
+      method: "GET",
+    }).then((res) => res.json());
+
+    return res;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const getPage = async (reportId, pageId) => {
+  try {
+    const res = await fetch(BASE_URL + `${reportId}/page/${pageId}/`, {
+      method: "GET",
+    }).then((res) => res.json());
+
+    return res;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const postPage = async (payload) => {
+  const { reportId, data } = payload;
+  const res = await fetch(BASE_URL + `${reportId}/page/`, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    throw new Error(`Error: ${res.status} ${res.statusText}`);
+  }
+
+  const report = await res.json();
+
+  return report;
+};
