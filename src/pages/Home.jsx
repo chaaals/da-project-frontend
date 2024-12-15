ad4c48cebfdcc14836f92f06dfa7b36c4838415f
Andre Villanueva <marcusandrev.mav@gmail.com>
2024-12-15 21:48:51 +0800

    feat: home and report pages changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
-import { useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
-import AppContext from "../context/app";
+import AppContext from '../context/app';
 
-import { getReports, postReport } from "../queries/report";
-import { postCSV } from "../queries/csv";
+import { getReports, postReport } from '../queries/report';
+import { postCSV } from '../queries/csv';
 
-import Table from "../components/Table";
-import Spinner from "../components/Spinner";
-import Button from "../components/Button";
-import DynamicTable from "../components/DynamicTable";
+import Table from '../components/Table';
+import Spinner from '../components/Spinner';
+import Button from '../components/Button';
+import DynamicTable from '../components/DynamicTable';
 
-import useTable from "../hooks/useTable";
+import useTable from '../hooks/useTable';
 
 function Home() {
   const {
@@ -25,6 +25,7 @@ function Home() {
     removeFile,
   } = useContext(AppContext);
   const [step, setStep] = useState(1);
+  const [fillStrategy, setFillStrategy] = useState('zero');
   const navigate = useNavigate();
 
   const { tableData, parseFetchedTable } = useTable({
@@ -32,7 +33,7 @@ function Home() {
   });
 
   const { isFetching, isLoading, data } = useQuery({
-    queryKey: ["reports"],
+    queryKey: ['reports'],
     queryFn: getReports,
   });
 
@@ -45,11 +46,11 @@ function Home() {
     onSuccess: (data) => {
       parseFetchedTable(data);
       setStep(2);
-      toast.success("CSV data has been cleaned!");
+      toast.success('CSV data has been cleaned!');
     },
     onError: (error) => {
-      toast.error("Oops, something went wrong.");
-      console.error("Oops, something went wrong.", error);
+      toast.error('Oops, something went wrong.');
+      console.error('Oops, something went wrong.', error);
     },
   });
 
@@ -59,30 +60,37 @@ function Home() {
       const { report } = data;
       navigate(`/report/${report.id}`);
       setSelectedFile(null);
-      toast.success("Successfully created a report.");
+      toast.success('Successfully created a report.');
     },
     onError: (error) => {
-      toast.error("Oops, something went wrong.");
-      console.error("Oops, something went wrong.", error);
+      toast.error('Oops, something went wrong.');
+      console.error('Oops, something went wrong.', error);
     },
   });
 
-  const reportColumns = ["id", "name"];
+  const reportColumns = ['id', 'name'];
+
+  const onSelectFillStrategy = (e) => {
+    const { value } = e.currentTarget;
+    console.log({ value });
+    setFillStrategy(value);
+  };
 
   const onClearFile = () => {
     removeFile();
     setStep(1);
+    setFillStrategy('zero');
   };
 
   const onCleanData = () => {
     const formData = new FormData();
-    formData.append("csv_upload", selectedFile);
+    formData.append('csv_upload', selectedFile);
 
-    cleanCSV(formData);
+    cleanCSV({ formData, fillStrategy });
   };
 
   const onAddReport = () => {
-    const [reportName] = selectedFile.name.split(".");
+    const [reportName] = selectedFile.name.split('.');
     const payload = {
       name: reportName,
       clean_columns: cleanedColumns,
@@ -91,19 +99,25 @@ function Home() {
     createReport(payload);
   };
 
+  useEffect(() => {
+    if (step === 2) {
+      onCleanData();
+    }
+  }, [fillStrategy]);
+
   return (
-    <section className="flex-col space-y-7 py-8 tablet:py-12 desktop:py-16 px-10 tablet:px-16 desktop:px-24 w-full">
-      <header className="flex gap-3 text-textPrimary place-items-center">
+    <section className='flex-col space-y-7 py-8 tablet:py-12 desktop:py-16 px-10 tablet:px-16 desktop:px-24 w-full'>
+      <header className='flex gap-3 text-textPrimary place-items-center'>
         <img
-          src="./images/search.svg"
-          alt="Search Icon"
-          className="size-7 desktop:size-9"
+          src='./images/search.svg'
+          alt='Search Icon'
+          className='size-7 desktop:size-9'
         />
-        <h1 className="text-textPrimary text-lg desktop:text-3xl font-inter font-bold">
+        <h1 className='text-textPrimary text-lg desktop:text-3xl font-inter font-bold'>
           Instant Reports with PowerBytes!
         </h1>
       </header>
-      <p className="text-textPrimary text-justify font-inter text-sm desktop:text-base">
+      <p className='text-textPrimary text-justify font-inter text-sm desktop:text-base'>
         <strong>PowerBytes</strong> is an easy-to-use web and mobile app that
         lets you upload CSV, generate interactive charts, and visualize your
         data in minutes. With Bytes, our AI assistant, you can ask questions
@@ -117,34 +131,34 @@ function Home() {
         <>
           <Button
             onClick={handleButtonClick}
-            style="w-full bg-colorSecondary rounded-xl h-64 text-textSecondary py-4 px-8 flex flex-col justify-center place-items-center"
+            style='w-full bg-colorSecondary rounded-xl h-64 text-textSecondary py-4 px-8 flex flex-col justify-center place-items-center'
           >
-            <div className="grid-cols-1">
+            <div className='grid-cols-1'>
               <img
-                src="./images/upload.svg"
-                alt="Search Icon"
-                className="size-auto max-w-12 max-h-12 place-self-center mx-auto"
+                src='./images/upload.svg'
+                alt='Search Icon'
+                className='size-auto max-w-12 max-h-12 place-self-center mx-auto'
               />
-              <span className="font-inter">
+              <span className='font-inter'>
                 <strong>Click to upload </strong>or drag and drop CSV
               </span>
             </div>
           </Button>
           {data && data.length > 0 ? (
             <>
-              <div className="h-1 bg-colorSecondary"></div>
-              <section className="flex-col space-y-7 w-full mt-16">
-                <header className="flex gap-3 text-textPrimary place-items-center">
+              <div className='h-1 bg-colorSecondary'></div>
+              <section className='flex-col space-y-7 w-full mt-16'>
+                <header className='flex gap-3 text-textPrimary place-items-center'>
                   <img
-                    src="./images/history.svg"
-                    alt="History Icon"
-                    className="size-7 desktop:size-9"
+                    src='./images/history.svg'
+                    alt='History Icon'
+                    className='size-7 desktop:size-9'
                   />
-                  <h1 className="text-textPrimary text-lg desktop:text-3xl font-inter font-bold">
+                  <h1 className='text-textPrimary text-lg desktop:text-3xl font-inter font-bold'>
                     History
                   </h1>
                 </header>
-                <div className="h-fit bg-colorSecondary rounded-xl py-6">
+                <div className='h-fit bg-colorSecondary rounded-xl py-6'>
                   {(isLoading || isFetching) && <Spinner />}
                   {!isLoading && !isFetching && (
                     <DynamicTable
@@ -161,64 +175,86 @@ function Home() {
           )}
         </>
       ) : (
-        <section className="flex-col space-y-9">
-          <header className="w-full bg-colorSecondary rounded-xl text-textPrimary flex h-12">
+        <section className='flex-col space-y-9'>
+          <header className='w-full bg-colorSecondary rounded-xl text-textPrimary flex h-12'>
             <Button
               onClick={handleButtonClick}
-              style="rounded-xl flex place-items-center text-textSecondary bg-[#4B5563] py-2 px-4 tablet:px-7 desktop:px-12 max-w-52"
+              style='rounded-xl flex place-items-center text-textSecondary bg-[#4B5563] py-2 px-4 tablet:px-7 desktop:px-12 max-w-52'
             >
-              <span className="font-inter text-textPrimary font-bold text-xs tablet:text-sm desktop:text-base text-nowrap">
+              <span className='font-inter text-textPrimary font-bold text-xs tablet:text-sm desktop:text-base text-nowrap'>
                 Choose a File
               </span>
             </Button>
-            <span className="font-inter italic text-[#979A9F] mx-4 truncate desktop:mx-7 my-auto text-sm tablet:text-base desktop:text-xl text-nowrap">
+            <span className='font-inter italic text-[#979A9F] mx-4 truncate desktop:mx-7 my-auto text-sm tablet:text-base desktop:text-xl text-nowrap'>
               {selectedFile.name}
             </span>
           </header>
 
           {tableData.length > 0 && <Table tableData={tableData} />}
 
-          <aside className="flex gap-4 justify-end">
-            <Button
-              onClick={onClearFile}
-              style="bg-[#C81E1E] h-12 rounded-lg px-4 "
-            >
-              <span className="font-inter text-white">Clear</span>
-            </Button>
-            {step === 1 && (
-              <Button
-                onClick={onCleanData}
-                style="w-32 bg-colorButton h-12 rounded-lg px-4"
+          <aside className='flex flex-wrap gap-4 justify-between'>
+            <div className='flex gap-4 items-center'>
+              <label
+                htmlFor='fill-strategy'
+                className='block mb-2 text-sm font-medium text-textPrimary'
               >
-                {isCleaning && <Spinner />}
-                {!isCleaning && (
-                  <span className="font-inter text-white text-nowrap">
-                    Clean Data
-                  </span>
-                )}
-              </Button>
-            )}
-            {step === 2 && (
-              <Button
-                onClick={onAddReport}
-                style="w-34 bg-colorButton h-12 rounded-lg px-4"
+                Fill strategy
+              </label>
+              <select
+                id='fill-strategy'
+                className='bg-gray-700 border border-gray-600 text-textPrimary text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-36 p-2.5'
+                value={fillStrategy}
+                onChange={onSelectFillStrategy}
               >
-                {isCreating && <Spinner />}
-                {!isCreating && (
-                  <span className="font-inter text-white text-nowrap">
-                    Create Report
-                  </span>
-                )}
+                {['forward', 'backward', 'zero', 'min', 'max'].map((opt) => (
+                  <option key={opt} value={opt}>
+                    {opt}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='flex gap-4'>
+              <Button
+                onClick={onClearFile}
+                style='bg-[#C81E1E] h-12 rounded-lg px-4 '
+              >
+                <span className='font-inter text-white'>Clear</span>
               </Button>
-            )}
+              {step === 1 && (
+                <Button
+                  onClick={onCleanData}
+                  style='w-32 bg-colorButton h-12 rounded-lg px-4'
+                >
+                  {isCleaning && <Spinner />}
+                  {!isCleaning && (
+                    <span className='font-inter text-white text-nowrap'>
+                      Clean Data
+                    </span>
+                  )}
+                </Button>
+              )}
+              {step === 2 && (
+                <Button
+                  onClick={onAddReport}
+                  style='w-34 bg-colorButton h-12 rounded-lg px-4'
+                >
+                  {(isCreating || isCleaning) && <Spinner />}
+                  {!isCreating && !isCleaning && (
+                    <span className='font-inter text-white text-nowrap'>
+                      Create Report
+                    </span>
+                  )}
+                </Button>
+              )}
+            </div>
           </aside>
         </section>
       )}
       <input
-        type="file"
-        accept=".csv"
+        type='file'
+        accept='.csv'
         ref={fileInputRef}
-        className="hidden"
+        className='hidden'
         onChange={handleFileChange}
       />
     </section>
